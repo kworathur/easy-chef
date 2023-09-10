cef48ed79f59a0030e11c4ac97bcd198d0784354
Keshav Worathur <keshavworathur@gmail.com>
2023-09-09 20:40:02 -0400

    feat: add endpoints to authenticate and refresh access token

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProfileView, ProfileEditView
+from .views import RegisterView, ProfileView, ProfileEditView, CookieTokenRefreshView, CookieTokenObtainPairView
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/edit/', ProfileEditView.as_view(), name='edit'),
-    path('profile/view/', ProfileView.as_view(), name='detail'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('profile/view/', ProfileView.as_view(), name='detail')
 ]
